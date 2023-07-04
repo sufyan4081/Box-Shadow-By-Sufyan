1a2ad475e788390aeac91438808b01b5db6b4b1e
Sufyanur Rasheed <120131185+sufyan4081@users.noreply.github.com>
2023-07-04 12:22:14 +0530

    Update ShadowBox.jsx

--- Box-Shadow/my-app/src/components/ShadowBox.jsx
+++ Box-Shadow/my-app/src/components/ShadowBox.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
-const ShadowBox = ({x, y, blur, spread, color, radius, height, width}) => {
+const ShadowBox = ({ x, y, blur, spread, color, radius, height, width, bcg }) => {
   return (
     <div className='shadow__box'>
-      <div className='shadow__div' style={{ boxShadow: `${x}px ${y}px ${blur}px ${spread}px ${color}`, borderRadius:`${radius}px`, height:`${height}vh`, width:`${width}vw`}} >
-      <div className='shadow__content'>
-        <p>box-shadow : {x}px {y}px {blur}px {spread}px {color}</p>
-        <p>-webkit-box-shadow:{x}px {y}px {blur}px {spread}px {color}</p>
-        <p>-moz-box-shadow:{x}px {y}px {blur}px {spread}px {color}</p>
-      </div>
+      <div className='shadow__div' style={{ boxShadow: `${x}px ${y}px ${blur}px ${spread}px ${color}`, borderRadius: `${radius}px`, height: `${height}vh`, width: `${width}vw`, backgroundColor: `${bcg}` }} >
+        <div className='shadow__content'>
+          <p>box-shadow : {x}px {y}px {blur}px {spread}px {color}</p>
+          <p>-webkit-box-shadow:{x}px {y}px {blur}px {spread}px {color}</p>
+          <p>-moz-box-shadow:{x}px {y}px {blur}px {spread}px {color}</p>
+        </div>
       </div>
     </div>
   )
